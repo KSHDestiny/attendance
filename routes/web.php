d1aaa805249bf29075d6ebe6735565b7f4549b98
KSHDestiny <kaungsathein.5899@gmail.com>
2023-10-07 20:41:00 +0630

    Email Verify

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/', function () {
     return view('auth.login');
 });
 
-Auth::routes();
+Auth::routes(['verify'=>true]);
 
 Route::resource('/employee',EmployeeController::class)->middleware('auth');
 
